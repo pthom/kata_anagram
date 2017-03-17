18bfaf1b504cc2cf255fbb37046788203219274c
Pascal Thomet <pthomet@ivsweb.com>
2017-03-17 12:25:55 +0100

    Scaffold / cmake project for TDD developement

---
+++ cmake/HunterInit.cmake
@@ -0,0 +1,5 @@
+include("${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake")
+HunterGate(
+    URL "https://github.com/ruslo/hunter/archive/v0.18.19.tar.gz"
+    SHA1 "9a97237ffb3128631dfedcf2fb892c4346d791ce"
+)
